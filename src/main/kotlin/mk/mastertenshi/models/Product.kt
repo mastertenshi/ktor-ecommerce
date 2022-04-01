db2baf8d60b61da257b4f9e2f975298f81bd653b
Angel Mitev <angelmitev@protonmail.com>
2022-04-01 06:16:30 +0200

    Add Product model

---
+++ src/main/kotlin/mk/mastertenshi/models/Product.kt
@@ -0,0 +1,49 @@
+package mk.mastertenshi.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Product(
+    val title: String,
+    val category: String,
+    val description: String,
+    val imgUrls: List<String>,
+    val type: Map<String, List<String>>,
+    val stock: MutableMap<String, Int> = mutableMapOf(),
+    val clicks: Int = 0
+) {
+    init {
+        initStock()
+    }
+
+    /**
+     * Initializes [stock] with all combinations of [type].values
+     * in the format of "1:2:3..." with the numbers indicating the List number
+     *
+     * Example:
+     *
+     *     Given the [type].values [["L", "XL"], ["white", "red", "yellow"]]
+     *     You would get "L:white", "L:red", "L:yellow", "XL:white",...
+     */
+    private fun initStock() {
+        if (type.isNotEmpty()) {
+            crawl()
+        }
+    }
+
+    private fun crawl(index: Int = 0, prefix: String = "") {
+        if (type.values.size - 1 == index) {
+            addAll(type.values.elementAt(index), prefix)
+        } else {
+            type.values.elementAt(index).forEach {
+                crawl(index + 1, "$prefix$it:")
+            }
+        }
+    }
+
+    private fun addAll(list: List<String>, prefix: String = "") {
+        list.forEach {
+            stock["$prefix$it"] = 0
+        }
+    }
+}
